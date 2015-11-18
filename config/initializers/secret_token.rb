15559c52c25338e2d3dbed0f9a5ffa248f0bb86c
dandrews <daniel.andrews@techstars.com>
2015-11-17 22:39:20 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PbcApplication::Application.config.secret_token = 'f0f08c3ea15ec0105aa441854cf3d3a94e3034fa342267eeec4b4587c7d5e346e969b8077a5e63cfcc61350d505cf44079c5fdcc93a695c7dc06799d3fe1e910'
